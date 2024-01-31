f9ff76b96cd9dd0c9da1da3cba5dc0467d8cc3fd
Hero Link 6 <123016274+HeroLink6@users.noreply.github.com>
2024-01-31 11:19:23 -0800

    Update consts.jsx

--- src/consts.jsx
+++ src/consts.jsx
@@ -1,4 +1,4 @@
-const obfuscation = true;
+const obfuscation = false;
 const github = "https://github.com/cognetwork-dev/Metallic";
 const discord = "https://discord.com/invite/yk33HZSZkU";
 const bareServerURL = new URL("/bare/", window.location);
